a87cd0f8393c470ae920b4eaac4a5bc4b71b6fd1
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-22 11:46:00 +0200

    Update

---
+++ payments-broker/src/test/java/pl/training/cloud/payments/PaymentsBrokerTests.java
@@ -0,0 +1,13 @@
+package pl.training.cloud.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsBrokerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
