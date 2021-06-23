1e0a29b46c4cb947da58e1adad31ee8309dc89d4
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-23 09:47:34 +0200

    Update

--- shop-service/src/main/java/pl/training/cloud/shop/ShopConfiguration.java
+++ shop-service/src/main/java/pl/training/cloud/shop/ShopConfiguration.java
@@ -45,9 +45,7 @@ public class ShopConfiguration {
 
     @Bean
     public Consumer<PaymentDto> paymentsChannel() {
-        return paymentDto -> {
-            log.info("Payment confirmation: " + paymentDto);
-        };
+        return paymentDto -> log.info("Payment confirmation: " + paymentDto);
     }
 
 }
