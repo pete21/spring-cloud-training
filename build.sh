c0076cde5264c73b7f495d75a98410a30214b670
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-23 11:50:35 +0200

    Update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 ./mvnw clean package -DskipTests=true
 docker build -t training/admin-server admin-server
 docker build -t training/configuration-server configuration-server
+docker build -t training/discovery-server discovery-server
