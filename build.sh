d66983c5cde105a6a4d585d8cac6ff5294830114
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-23 12:12:36 +0200

    Update

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 docker build -t training/admin-server admin-server
 docker build -t training/configuration-server configuration-server
 docker build -t training/discovery-server discovery-server
+docker build -t training/gateway-server gateway-server
