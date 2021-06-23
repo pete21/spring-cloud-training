5ec3691f8e181d3563d2a8a93f9018a9ec47e9f4
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-23 10:10:28 +0200

    Update

---
+++ admin-server/src/test/java/pl/training/cloud/admin/AdminServerTests.java
@@ -0,0 +1,13 @@
+package pl.training.cloud.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
