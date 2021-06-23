6d68454a69e0dece7f30e8927723eaddce0f795f
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-23 12:43:59 +0200

    Update

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ docker build -t training/discovery-server discovery-server
 docker build -t training/gateway-server gateway-server
 docker build -t training/payments-broker payments-broker
 docker build -t training/payments-service payments-service
+docker build -t training/shop-service shop-service
