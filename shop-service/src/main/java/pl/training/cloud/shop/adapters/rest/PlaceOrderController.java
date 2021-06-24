7cbeca308145288d6c013fe240528a8b7c96165c
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-24 09:58:08 +0200

    Update

--- shop-service/src/main/java/pl/training/cloud/shop/adapters/rest/PlaceOrderController.java
+++ shop-service/src/main/java/pl/training/cloud/shop/adapters/rest/PlaceOrderController.java
@@ -2,12 +2,10 @@ package pl.training.cloud.shop.adapters.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.training.cloud.shop.ports.usecases.PlaceOrderUseCase;
 
+@RequestMapping("orders")
 @RestController
 @RequiredArgsConstructor
 public class PlaceOrderController {
@@ -15,7 +13,7 @@ public class PlaceOrderController {
     private final PlaceOrderUseCase placeOrderUseCase;
     private final RestMapper mapper;
 
-    @PostMapping("orders")
+    @PostMapping
     public ResponseEntity<Void> placeOrder(@RequestBody OrderDto orderDto) {
         var order = mapper.toDomain(orderDto);
         placeOrderUseCase.place(order);
