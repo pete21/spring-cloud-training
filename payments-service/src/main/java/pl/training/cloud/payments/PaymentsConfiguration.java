6782393a477cf52194763157f3b8608d1fec886e
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-25 14:09:30 +0200

    Update

--- payments-service/src/main/java/pl/training/cloud/payments/PaymentsConfiguration.java
+++ payments-service/src/main/java/pl/training/cloud/payments/PaymentsConfiguration.java
@@ -1,10 +1,21 @@
 package pl.training.cloud.payments;
 
 import org.mapstruct.factory.Mappers;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.client.RestTemplate;
 import pl.training.cloud.commons.money.FastMoneyMapper;
 import pl.training.cloud.payments.adapters.rest.PaymentDto;
@@ -22,8 +33,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.io.Serializable;
 import java.util.function.Supplier;
 
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
+
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+@EnableCaching
 @EnableAspectJAutoProxy
 @Configuration
 public class PaymentsConfiguration {
@@ -33,6 +49,25 @@ public class PaymentsConfiguration {
         return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("pl.training.cloud.payments.adapters.rest"))
+                .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Serializable> stringSerializableRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        var template = new RedisTemplate<String, Serializable>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        var config = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(fromSerializer(new StringRedisSerializer()))
+            .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
                 .build();
     }
 
--- payments-service/src/main/java/pl/training/cloud/payments/adapters/rest/GetPaymentController.java
+++ payments-service/src/main/java/pl/training/cloud/payments/adapters/rest/GetPaymentController.java
@@ -1,19 +1,26 @@
 package pl.training.cloud.payments.adapters.rest;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import pl.training.cloud.payments.ports.usecases.GetPaymentUseCase;
 
+@CacheConfig(cacheNames = "payments")
 @RestController
 @RequiredArgsConstructor
+@Log
 public class GetPaymentController implements GetPaymentApi {
 
     private final GetPaymentUseCase getPaymentUseCase;
     private final RestMapper mapper;
 
+    @Cacheable(cacheNames = "payments")
     @Override
     public ResponseEntity<PaymentDto> findById(String id) {
+        log.info("### Not using cache");
         var payment = getPaymentUseCase.findById(id);
         var paymentDto = mapper.toDto(payment);
         return ResponseEntity.ok(paymentDto);
